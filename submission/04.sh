86f62430d638f88759525c0dee6aceec17b2a539
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-04-16 00:54:45 +0100

    add: csv script to 05

--- submission/04.sh
+++ submission/04.sh
@@ -1,10 +1,19 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 timestamp=1495584032
+
+# Get the hex of timestamp
 hextimestamp=$(printf '%08x\n' $timestamp | sed 's/^\(00\)*//')
+
+# Get the little endian format of the hex
 lehex=$(echo "$hextimestamp" | sed 's/../& /g' | awk '{for(i=NF;i>0;i--) printf "%s", $i; print ""}')
-#echo $lehex
+
+# Get pubkey
 pubkeyhash=$(echo $publicKey | xxd -r -p | openssl sha256 -binary | openssl rmd160 | cut -d' ' -f2)
-#echo $pubkeyhash
+
+# Get serialized script
+# Script format : OP_PUSHDATA 4bytes <TIMESTAMP> OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 OP_PUSHDATA 20bytes <PUBKEY_HASH> OP_EQUALVERIFY OP_CHECKSIG 
 serializedScript="04"$lehex"b17576a914"$pubkeyhash"88ac"
+
+# Echo CLTV Script
 echo $serializedScript
--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,29 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+# publicKey
+publickey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+# Block height or time
+BLOCKHEIGHT=150
+#echo $BLOCKHEIGHT
+
+# Hex of blockheight
+hexBH=$(printf '%08x\n' $BLOCKHEIGHT | sed 's/^\(00\)*//')
+hexfirst=$(echo $hexBH | cut -c1)
+[[ 0x$hexfirst -gt 0x7 ]] && hexBH="00"$hexBH
+#echo $hexBH
+
+# Get the little endian format of the hex
+lehexBH=$(echo "$hexBH" | sed 's/../& /g' | awk '{for(i=NF;i>0;i--) printf "%s", $i; print ""}')
+#echo $lehexBH
+
+# Blockheight size
+SizeBH=$(echo -n $lehexBH | wc -c | awk '{print $1/2}')
+#echo $SizeBH
+
+# Get pubkey
+pubkeyhash=$(echo $publickey | xxd -r -p | openssl sha256 -binary | openssl rmd160 | cut -d' ' -f2)
+#echo $pubkeyhash
+
+# Script format : OP_PUSHDATA 4bytes <time> OP_CHECKSEQUENCEVERIFY OP_DROP OP_DUP OP_HASH160 OP_PUSHDATA 20bytes <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+CSV_Script="0$SizeBH"$lehexBH"b275766a914"$pubkeyhash"88ac"
+echo $CSV_Script
