22a7c5861b55526a7ca1c10939d0b0666a3cd1a8
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-04-16 00:08:15 +0100

    fix: correct pubckey variable name

--- submission/04.sh
+++ submission/04.sh
@@ -4,7 +4,7 @@ timestamp=1495584032
 hextimestamp=$(printf '%08x\n' $timestamp | sed 's/^\(00\)*//')
 lehex=$(echo "$hextimestamp" | sed 's/../& /g' | awk '{for(i=NF;i>0;i--) printf "%s", $i; print ""}')
 #echo $lehex
-pubkeyhash=$(echo $publickey | xxd -r -p | openssl sha256 -binary | openssl rmd160 | cut -d' ' -f2)
+pubkeyhash=$(echo $publicKey | xxd -r -p | openssl sha256 -binary | openssl rmd160 | cut -d' ' -f2)
 #echo $pubkeyhash
 serializedScript="04"$lehex"b17576a914"$pubkeyhash"88ac"
 echo $serializedScript
